bbf7c80063c677b06d7473b27eb438d863cf719b
Soumyakusuma <soumyakusuma2@gmail.com>
2025-05-18 11:01:56 +0530

    nginx install

--- conditions.sh
+++ conditions.sh
@@ -7,9 +7,43 @@ then
 echo "only root user have the permission"
 exit 1
 else
-echo "you  have root access"
-fi
+echo "you have root access"
 dnf list installed nginx 
+if [$? -eq 0]
+then
+echo "nginx already installed"
+exit 1
+else
+dnf install nginx
+if [ $? eq 0 ]
+then 
+echo "nginx install is success"
+else
+echo "nginx install is failure"
+fi
+fi
+fi
+
+
+
+
+
+
+
+
+if [ $? -ne 0 ]
+dnf install nginx -y
+if [ $? eq 0 ]
+then 
+echo "nginx install is success"
+else
+echo "nginx install is failure"
+
+else
+echo "nginx already installed"
+fi
+fi
+
 if [ $? -eq 0 ]
 then
 echo "nginx already installed"
