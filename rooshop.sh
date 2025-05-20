f6d02e381c41bff29e69936e88fa0b5d2d824675
Soumyakusuma <soumyakusuma2@gmail.com>
2025-05-20 11:16:17 +0530

    nginx install

--- rooshop.sh
+++ rooshop.sh
@@ -9,7 +9,7 @@ DOMAIN_NAME="pract.site"
 for instance in ${INSTANCES[@]}
 do
 INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-06dc2b0cd3ece8e62 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
-if [ $instance != 'frontend']
+if [ $instance != 'frontend' ]
 then
  IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
  echo "ip address is $IP , $INSTANCE_ID"
