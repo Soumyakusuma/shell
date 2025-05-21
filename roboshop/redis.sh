003f1fa04cc7e48f4e02fe3824b291645e047093
Soumyakusuma <soumyakusuma2@gmail.com>
2025-05-21 20:02:19 +0530

    nginx install

--- roboshop/redis.sh
+++ roboshop/redis.sh
@@ -25,18 +25,18 @@ VALIDATE(){
  fi
 }
 
-dnf module disable redis -y
+dnf module disable redis -y &>>$LOG_FILE
 VALIDATE $? "disable redis"
 
-dnf module enable redis:7 -y
+dnf module enable redis:7 -y &>>$LOG_FILE
 VALIDATE $? "enable redis"
 
-dnf install redis -y 
+dnf install redis -y  &>>$LOG_FILE
 VALIDATE $? "install redis"
 
 sed -ie 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
 VALIDATE $? "change ip and protected mode in redis conf file"
 
 systemctl enable redis 
-systemctl start redis 
+systemctl start redis &>>$LOG_FILE
 VALIDATE $? "enable and start redis"
