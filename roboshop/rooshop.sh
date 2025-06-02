b9ac21ebb05423ebc206eca58780e9a2bde36da1
Soumyakusuma <soumyakusuma2@gmail.com>
2025-06-02 20:24:15 +0530

    nginx install

--- roboshop/rooshop.sh
+++ roboshop/rooshop.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-06dc2b0cd3ece8e62"
+SG_ID="sg-099e213b8903ae6d9"
 #INSTANCES=("mongodb" "frontend" "catalogue" "redis" "mysql" "rabbitmq" "user" "cart" "shipping" "payment" "dispatch" )
-ZONE_ID="Z0349103U6EUQ8TEV3VX"
+ZONE_ID="Z03148573A2NAXD6GP1WZ"
 DOMAIN_NAME="pract.site"
 
 for instance in $@
 do
-INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-06dc2b0cd3ece8e62 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-099e213b8903ae6d9 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
 if [ $instance != 'frontend' ]
 then
  IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
